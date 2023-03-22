4504f6defc41980fd43e66083163b45a7c0f9df1
ryan605 <langatryan26@gmail.com>
2023-03-22 13:25:56 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+
+    def show
+        # byebug
+        user = User.find_by(id: params[:id])
+        render json: user, include: :items
+    end
+
 end
